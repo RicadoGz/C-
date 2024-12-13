72918f79f4b74c59afeac3cffc308797155805a7
RicardoGz <163072550+RicadoGz@users.noreply.github.com>
2024-12-13 14:50:37 -0500

    basic operator of the C#
    basic operator of the C#

---
+++ MyfirstAPp/myfirstWinFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+namespace myfirstWinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*int x = 2;
+            //MessageBox.Show(x.ToString());
+            int y = 3;
+            string fuck = "tx wo cao ni ma";
+            MessageBox.Show(fuck);*/
+            int x = 5;
+            int y = ++x;
+            MessageBox.Show(y.ToString());
+            MessageBox.Show(x.ToString());
+            y = x++;
+            MessageBox.Show(y.ToString());
+            MessageBox.Show(x.ToString());
+
+
+        }
+    }
+}
---
+++ MyfirstAPp/myfirstWinFormsApp1/models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myfirstWinFormsApp1.models
+{
+    internal class User
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="y"></param>
+        void sayHello(int y)
+        {
+
+        }
+    }
+}
